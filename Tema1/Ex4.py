adcaf92db23855222bb884d7748ed894e0b3a269
Tugui Madalina <tuguimadalinaa@gmail.com>
2021-03-19 16:12:27 +0200

    refactor ex4

--- Tema1/Ex4.py
+++ Tema1/Ex4.py
@@ -1,18 +1,24 @@
 import json
 
 
+def go_keys(data, key):
+    if type(data) == dict:
+        if len(data) > 1:
+            new_string = ""
+            for k, v in data.items():
+                new_string += str(go_keys(v, str(key) + "." + str(k)))
+                new_string += "\n"
+            return new_string
+        else:
+            return [str(key) + "." + str(x[0]) + "." + str(x[1]) for x in data.items()][0]
+    return str(key) + "." + str(data)
+
+
 def print_dict(data):
     for key, value in data.items():
-
         if type(value) == dict:
-            if len(value.items()) > 1:
-                for k, v in value.items():
-                    print(str(key), end=".")
-                    print_dict(dict({k: v}))
-
-            else:
-                print(str(key), end=".")
-                print_dict(value)
+            result = go_keys(value, key)
+            print(result)
         else:
             print(str(key), end=".")
             print(str(value), end="\n")
